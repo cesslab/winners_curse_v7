d108a57e57054b00e15dfb4c4fa3b04283a4fd58
aaruff <anwarruff@gmail.com>
2021-04-27 16:01:46 -0400

    remove valuation from app sequence

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from os import environ, getenv
 SESSION_CONFIGS = [
     {
         "name": "wc",
-        "app_sequence": ["instructions", "bid", "questions", "valuation"],
+        "app_sequence": ["instructions", "bid", "questions"],
         "num_demo_participants": 2,
         "treatment": "cp",
         "lottery_1": "1",
