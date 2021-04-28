d15c1a291f80c0bf4213d794475d6815ade3ba3b
aaruff <anwarruff@gmail.com>
2021-04-28 11:31:57 -0400

    remove unused model functions from import

--- instructions/__init__.py
+++ instructions/__init__.py
@@ -2,9 +2,7 @@ from otree.api import BaseGroup, BaseSubsession, models, BasePlayer
 
 from exp.models import (
     BidHistoryPlayer,
-    create_player_bid_histories,
     ExperimentSubSession,
-    save_bid_history_for_all_players
 )
 
 
