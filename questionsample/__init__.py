8a10181c6f6b2806a2abd95b01792aa87bfa87cb
aaruff <anwarruff@gmail.com>
2021-04-28 18:39:14 -0400

    added sample parameters

--- questionsample/__init__.py
+++ questionsample/__init__.py
@@ -6,12 +6,12 @@ from exp.models import (
 )
 
 from questions.views import (
-    Instructions,
     Worth,
     Update,
     Probability,
     UpdatedWorth,
 )
+from .views import Instructions
 from .constants import Constants
 
 doc = """
@@ -50,19 +50,19 @@ class Player(BasePlayer, BidHistoryPlayer):
     others_group_id = models.IntegerField(initial=0)
     others_player_id = models.IntegerField(initial=0)
     others_bid = models.IntegerField(initial=0)
-    signal = models.IntegerField(initial=50)
-    alpha = models.IntegerField(inital=50)
-    beta = models.IntegerField(inital=70)
-    epsilon = models.IntegerField(initial=50)
-    ticket_value_before = models.IntegerField(initial=50)
-    ticket_probability = models.IntegerField(initial=50)
-    fixed_value = models.IntegerField(initial=50)
-    ticket_value_after = models.IntegerField(initial=50)
-    previous_highest_bid = models.IntegerField(initial=50)
+    signal = models.IntegerField(initial=23)
+    alpha = models.IntegerField(initial=10)
+    beta = models.IntegerField(initial=30)
+    epsilon = models.IntegerField(initial=4)
+    ticket_value_before = models.IntegerField(initial=0)
+    ticket_probability = models.IntegerField(initial=75)
+    fixed_value = models.IntegerField(initial=75)
+    ticket_value_after = models.IntegerField(initial=0)
+    previous_highest_bid = models.IntegerField(initial=0)
     # Player Bid History
-    rounds_per_lottery = models.IntegerField()
-    player_bid_history_id = models.IntegerField()
-    part_round_number = models.IntegerField()
+    rounds_per_lottery = models.IntegerField(initial=1)
+    player_bid_history_id = models.IntegerField(initial=0)
+    part_round_number = models.IntegerField(initial=1)
 
 
 page_sequence = [Instructions, Update, Worth, Probability, UpdatedWorth]
