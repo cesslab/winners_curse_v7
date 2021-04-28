f35cd6980e90feb8224fc29c82b147048f4562ea
aaruff <anwarruff@gmail.com>
2021-04-28 13:08:28 -0400

    add the sample apps to the app sequence

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from os import environ, getenv
 SESSION_CONFIGS = [
     {
         "name": "wc",
-        "app_sequence": ["instructions", "bid", "questions"],
+        "app_sequence": ["instructions", "bidsample", "bid", "questionsample", "questions"],
         "num_demo_participants": 2,
         "treatment": "cp",
         "lottery_1": "1",
