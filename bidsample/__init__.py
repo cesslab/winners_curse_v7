65ee0215b10b88c85aac7acd8d85bf00632d9a2a
aaruff <anwarruff@gmail.com>
2021-04-28 11:31:13 -0400

    add bid sample player fields

--- bidsample/__init__.py
+++ bidsample/__init__.py
@@ -1,5 +1,11 @@
 from otree.api import *
 
+from exp.models import (
+    BidHistoryPlayer,
+)
+
+from .views import Instructions, Bid, Outcome
+
 c = Currency
 
 doc = """
@@ -21,8 +27,33 @@ class Group(BaseGroup):
     pass
 
 
-class Player(BasePlayer):
-    pass
+class Player(BasePlayer, BidHistoryPlayer):
+    bid = models.IntegerField(min=0, max=100)
+    tie = models.BooleanField()
+    winner = models.BooleanField()
+    # Bid History
+    bid_history_id = models.IntegerField(initial=0)
+    previous_session_id = models.IntegerField(initial=0)
+    lottery_id = models.IntegerField(initial=0)
+    treatment = models.StringField(choices=["cp", "cv"], initial="cp")
+    lottery_round_number = models.IntegerField(initial=1)
+    lottery_order = models.IntegerField(initial=1)
+    others_group_id = models.IntegerField(initial=1)
+    others_player_id = models.IntegerField(initial=1)
+    others_bid = models.IntegerField(initial=50)
+    signal = models.IntegerField(initial=90)
+    alpha = models.IntegerField(initial=10)
+    beta = models.IntegerField(initial=80)
+    epsilon = models.IntegerField(initial=30)
+    ticket_value_before = models.IntegerField(initial=30)
+    ticket_probability = models.IntegerField(initial=30)
+    fixed_value = models.IntegerField(initial=15)
+    ticket_value_after = models.IntegerField(initial=30)
+    previous_highest_bid = models.IntegerField(initial=99)
+    # Player Bid History
+    rounds_per_lottery = models.IntegerField(initial=1)
+    player_bid_history_id = models.IntegerField(initial=0)
+    part_round_number = models.IntegerField(initial=0)
 
 
 # PAGES
@@ -30,4 +61,4 @@ class MyPage(Page):
     pass
 
 
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [Instructions, Bid, Outcome]
