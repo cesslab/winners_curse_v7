c3a3a929c68b51d1d3f86ed85d53447f07d397ed
aaruff <anwarruff@gmail.com>
2021-05-06 19:27:19 -0400

    add previous experiment's questionnaire

---
+++ questionnaire/__init__.py
@@ -0,0 +1,51 @@
+from otree.api import *
+
+from .views import PartOne, PartTwoA, PartTwoB, PartTwoC, PartThree
+c = Currency
+
+doc = """
+Questionnaire
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'questionnaire'
+    players_per_group = None
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    gender = models.StringField()
+    age = models.IntegerField(min=18, max=100)
+    major = models.StringField()
+    q1a = models.IntegerField()
+    q1b = models.IntegerField()
+    q1c = models.IntegerField()
+    q1d = models.IntegerField()
+    q1e = models.IntegerField()
+    q1exp = models.LongStringField()
+    q2 = models.IntegerField()
+    q3 = models.IntegerField()
+    q3exp = models.LongStringField()
+    q4 = models.LongStringField()
+    q5 = models.IntegerField(choices=[[1, 'I prefer lottery A.'], [2, 'I prefer lottery B.'], [3, 'I don\'t care.']],
+                             widget=widgets.RadioSelect)
+    q5exp = models.LongStringField()
+    q6 = models.IntegerField(choices=[[1, 'I prefer lottery A.'], [2, 'I prefer lottery B.'], [3, 'I don\'t care.']],
+                             widget=widgets.RadioSelect)
+    q6exp = models.LongStringField()
+    q7 = models.IntegerField()
+    q8 = models.IntegerField()
+    q9 = models.IntegerField()
+
+page_sequence = [
+    PartOne, PartTwoA, PartTwoB, PartTwoC, PartThree
+]
