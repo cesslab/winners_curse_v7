78c6ba8df725a9ca0837e00c81c7022926c1d863
aaruff <anwarruff@gmail.com>
2021-05-06 06:33:28 -0400

    Add worth payoff view and template

--- payoff/__init__.py
+++ payoff/__init__.py
@@ -31,5 +31,9 @@ class BidPayoff(Page):
     def vars_for_template(player):
         return player.participant.vars['bid_payoff_data']
 
+class WorthPayoff(Page):
+    @staticmethod
+    def vars_for_template(player):
+        return player.participant.vars['worth_payoff_data']
 
 page_sequence = [BidPayoff]
