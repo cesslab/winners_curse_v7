cd6c4a9443715b13dbd1f30fbc2cccf6a1dbe549
aaruff <anwarruff@gmail.com>
2021-06-16 18:34:24 -0400

    Add explain question

--- questionnaire/views.py
+++ questionnaire/views.py
@@ -8,7 +8,7 @@ class PartOne(Page):
 
 class PartTwo(Page):
     form_model = 'player'
-    form_fields = ['p2_q1', 'p2_q1_explain', 'p2_q2', 'p2_q3', 'p2_q4', 'p2_q4_explain', 'p2_q5']
+    form_fields = ['p2_q1', 'p2_q1_explain', 'p2_q2', 'p2_q3', 'p2_q4', 'p2_q4_explain', 'p2_q5', 'p2_q5_explain']
 
 
 class PartThree(Page):
