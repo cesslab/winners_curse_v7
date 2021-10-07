6005027fffaacc7d57d8a889f80fd230e2af781d
aaruff <anwarruff@gmail.com>
2021-10-07 10:33:04 -0400

    update quiz

--- quiz/__init__.py
+++ quiz/__init__.py
@@ -24,89 +24,68 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer, BidHistoryPlayer):
-    q1 = models.IntegerField(
-        label='1. Suppose that you are not the highest bidder in an auction. How much do you have to pay?',
-        choices=[[1, 'Your bid'], [2, 'The highest bid'], [3, 'Nothing']],
-        widget=widgets.RadioSelect
-    )
-    cp_q2 = models.IntegerField(
-        label='2. Which of the following alternatives is correct?',
-        choices=[
-            [1, 'All four bidders bid for the same lottery ticket with the same Selected Probability.'],
-            [2, 'All four bidders bid for lottery tickets with different Selected Probabilities.'],
-            [3, 'All four bidders bid for lottery tickets with possibly different Selected Probabilities.']],
-        widget=widgets.RadioSelect
-    )
-    cv_q2 = models.IntegerField(
-        label='2. Which of the following alternatives is correct?',
-        choices=[
-            [1, 'All four bidders bid for the same lottery ticket with the same Selected Value.'],
-            [2, 'All four bidders bid for lottery tickets with different Selected Value.'],
-            [3, 'All four bidders bid for lottery tickets with possibly different Selected Value.']],
-        widget=widgets.RadioSelect
-    )
-    cp_q3 = models.IntegerField(
-        label='3. Which of the following alternatives is correct?',
-        choices=[
-            [1, 'All four bidders receive the same signal about the same Selected Probability.'],
-            [2, 'All four bidders receive possibly different signals about the same Selected Probability.'],
-            [3, 'All four bidders receive different signals because Selected Probabilities differ for each of them.']],
-        widget=widgets.RadioSelect
-    )
-    cv_q3 = models.IntegerField(
-        label='3. Which of the following alternatives is correct?',
-        choices=[
-            [1, 'All four bidders receive the same signal about the same Selected Value.'],
-            [2, 'All four bidders receive possibly different signals about the same Selected Value.'],
-            [3, 'All four bidders receive different signals because Selected Value differ for each of them.']],
-        widget=widgets.RadioSelect
-    )
-    cp_q4 = models.IntegerField(
-        label='4. Suppose you win the lottery ticket in a given auction. What are your earnings from this auction?',
-        choices=[
-            [1, 'The non-zero value of the lottery minus your bid.'],
-            [2, 'The outcome of the lottery minus your bid.']],
-        widget=widgets.RadioSelect
-    )
-    cv_q4 = models.IntegerField(
-        label='4. Suppose you win the lottery ticket in a given auction. What are your earnings from this auction?',
-        choices=[
-            [1, 'The Selected Value of the lottery minus your bid.'],
-            [2, 'The outcome of the lottery minus your bid.']],
-        widget=widgets.RadioSelect
-    )
-    cp_q5 = models.IntegerField(
-        label='5. Suppose that you receive a signal 30 that is at most 4 percentage points away from the Selected Probability. What could be the Selected Probability?',
+    cp_q1 = models.IntegerField(
+        label='1. Suppose that you receive a signal 30 that is at most 4 percentage points away from the Selected Probability. What could be the Selected Probability?',
         choices=[
             [1, '18%'],
             [2, '28%'],
             [3, '38%']],
         widget=widgets.RadioSelect
     )
-    cv_q5 = models.IntegerField(
-        label='5. Suppose that you receive a signal of 30 that is at most 4 units away from the Selected Value. What could be the Selected Value?',
+    cv_q1 = models.IntegerField(
+        label='1. Suppose that you receive a signal of 30 that is at most 4 units away from the Selected Value. What could be the Selected Value?',
         choices=[
             [1, '18'],
             [2, '28'],
             [3, '38']],
         widget=widgets.RadioSelect
     )
-    cp_q6 = models.IntegerField(
-        label='6. Suppose the Selected Probability is 30%. The outcome of the lottery in credits...',
+
+    cp_q2 = models.IntegerField(
+        label='2. Suppose the Selected Probability is 30%. The outcome of the lottery in credits...',
         choices=[
             [1, 'can only be 70'],
             [2, '21 ( = 30% x 70 + 70% x 0)'],
             [3, 'can be 0 or 70']],
         widget=widgets.RadioSelect
     )
-    cv_q6 = models.IntegerField(
-        label='6. Suppose the Selected Value is 30.The outcome of the lottery in credits...',
+    cv_q2 = models.IntegerField(
+        label='2. Suppose the Selected Value is 30.The outcome of the lottery in credits...',
         choices=[
             [1, 'can only be 30'],
             [2, '21 ( = 30 x 70% +  0 x 30%)'],
             [3, 'can be 0 or 30']],
         widget=widgets.RadioSelect
     )
+    q3 = models.IntegerField(
+        label='3. Which of the following alternatives is correct?',
+        choices=[
+            [1, 'There will be four signals, but you will see none of them.'],
+            [2, 'There will be four signals, but you will see only one of them.'],
+            [3, 'There will be four signals, but you will see only two of them.'],
+            [4, 'You will see four signals.']],
+        widget=widgets.RadioSelect
+    )
+
+    cp_q4 = models.IntegerField(
+        label='3. Which of the following alternatives is correct?',
+        choices=[
+            [1, 'The four signals are all the same, so if you see one of them, you know all of them.'],
+            [2, 'All four signals are possibly different but contain information about the same Selected Probabilities.'],
+            [3, 'The four signals are all different because each one of them is about a different Selected Probabilities.']],
+        widget=widgets.RadioSelect
+    )
+    cv_q4 = models.IntegerField(
+        label='3. Which of the following alternatives is correct?',
+        choices=[
+            [1, 'The four signals are all the same, so if you see one of them, you know all of them.'],
+            [2,
+             'All four signals are possibly different but contain information about the same Selected Value.'],
+            [3,
+             'The four signals are all different because each one of them is about a different Selected Value.']],
+        widget=widgets.RadioSelect
+    )
+
     signal = models.IntegerField(initial=30)
     alpha = models.IntegerField(initial=20)
     beta = models.IntegerField(initial=40)
